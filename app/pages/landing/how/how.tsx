0c0ee4ae830970c4798fef4bc6490e51a883c5fb
Joel K George <joelk9895@gmail.com>
2024-02-04 10:53:34 +0530

    First commit

--- app/pages/landing/how/how.tsx
+++ app/pages/landing/how/how.tsx
@@ -1,6 +1,6 @@
 export default function How() {
   return (
-    <section className="flex flex-col items-center justify-center w-[100svw] h-[100svh] bg-black bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]">
+    <section className="flex flex-col items-center justify-center w-[100svw] h-[100svh] bg-black bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] text-white">
       <h1 className="text-8xl font-bold text-center mt-10">
         how it works<span className="text-[#DBF72C] text-[5rem]">?</span>
       </h1>
